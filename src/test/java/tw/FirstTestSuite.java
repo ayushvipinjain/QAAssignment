93bcf5a5cfb39aebcd07ede34f0e7b5e47aadd95
Ayush Jain <jayush@thoughtworks.com>
2020-02-05 14:07:36 +0530

    Added more tests

--- src/test/java/tw/FirstTestSuite.java
+++ src/test/java/tw/FirstTestSuite.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,14 +14,17 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 public class FirstTestSuite {
 
     @Test(description = "Search Quality Analyst and verify the no of results dispayed")
-    public  void FirstTestCase() throws InterruptedException {
+    public  void FirstTestCase() {
 
         WebDriver driver = DriverFactory.getDriver(DriverType.valueOf(System.getProperty("browser").toUpperCase()));
 
         driver.navigate().to("https://www.thoughtworks.com");
+        driver.manage().timeouts().implicitlyWait(50000, MILLISECONDS);
         driver.manage().window().maximize();
 
         driver.findElement(By.xpath("//span[text()='Search']")).click();
@@ -34,14 +38,55 @@ public class FirstTestSuite {
         WebElement nextLink = driver.findElement(By.xpath("//a[text()='Next']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", nextLink);
 
-        if(nextLink.isDisplayed()) {
+        if(new WebDriverWait(driver, 15).until(ExpectedConditions.visibilityOf(nextLink)).isDisplayed()) {
             nextLink.click();
 
             WebDriverWait wait = new WebDriverWait(driver, 15);
             WebElement previousLink =wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='Previous']")));
-
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", previousLink);
-            Assert.assertEquals(previousLink.isDisplayed(), true);
+            Assert.assertTrue(previousLink.isDisplayed());
+        }
+
+        driver.quit();
+    }
+
+    @Test(description = "Search Jobs inside careers section")
+    public  void seachJobsUnderCareers() {
+
+        WebDriver driver = DriverFactory
+                .getDriver(DriverType.valueOf(System.getProperty("browser").toUpperCase()));
+        driver.manage().timeouts().implicitlyWait(50000, MILLISECONDS);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.thoughtworks.com");
+
+        Actions actions = new Actions(driver);
+
+        driver.findElement(By.xpath("//text()[.='Careers']/ancestor::button")).click();
+        driver.findElement(By.xpath("//div[@id='careers']//a[text()='Search jobs']")).click();
+
+        WebDriverWait wait  = new WebDriverWait(driver,10);
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search-query")));
+        searchBox.sendKeys("Quality Analyst");
+
+        WebElement selectCity = driver.findElement(By.xpath("//span[text()='City']"));
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, (document.body.scrollHeight)/5)");
+        wait.until(ExpectedConditions.visibilityOf(selectCity));
+
+        selectCity.click();
+        WebElement menuItem = selectCity.findElement(By.xpath("following-sibling::div"));
+        List<WebElement> menuitems = menuItem.findElements(By.xpath("//label[@class='multi-select-menuitem']"));
+        for (WebElement item:menuitems) {
+            if(item.isDisplayed())
+            {
+                String value  = item.getText().trim();
+                 if(value.equals("Pune")){
+                     actions.moveToElement(item);
+                     if(!item.isSelected()) {
+                         item.click();
+                         selectCity.click();
+                     }
+                 }
+            }
         }
 
         driver.quit();
